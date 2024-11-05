ec1cb7fb548e54fc92e52789512f302380e40c56
LaerkeI <imeland3@gmail.com>
2024-11-05 02:42:38 +0100

    Week 44 - API Gateways "ApiGateway" has been split into the projects "ApiGateway" which has the Ocelot Gateway setup, and "AuthenticationService" which creates a JWT token for the requests to use. Changed to fit the tutorial created by the professor.

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,83 +1,53 @@
 namespace ApiGateway
 {
+    using System.Text;
     using Microsoft.AspNetCore.Authentication.JwtBearer;
-    using Microsoft.AspNetCore.Authorization;
     using Microsoft.IdentityModel.Tokens;
     using Ocelot.DependencyInjection;
     using Ocelot.Middleware;
-    using System.IdentityModel.Tokens.Jwt;
-    using System.Security.Claims;
-    using System.Text;
-    using System.Threading.Tasks;
-
     public class Program
     {
         public static async Task Main(string[] args)
         {
+            const string SecurityKey = "MyClientSecretThatIsDefinitelyNotTooShort";
+
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add JWT Authentication
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
+            // Load Ocelot configuration from the ocelot.json file
+            builder.Configuration.AddJsonFile("ocelot.json", false, false);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(
+                JwtBearerDefaults.AuthenticationScheme, options =>
                 {
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
-                        ValidateIssuer = false,  // Skipping issuer validation for simplicity
-                        ValidateAudience = false,  // Skipping audience validation for simplicity
-                        ValidateLifetime = true,  // Ensure the token hasn’t expired
-                        ValidateIssuerSigningKey = true,  // Validate the token signature
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]))
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = false,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecurityKey))
                     };
-                });
-
+                }  
+            );
+            
             // Add Ocelot to the services
-            builder.Services.AddOcelot();
-
-            // Load Ocelot configuration from the ocelot.json file
-            builder.Configuration.AddJsonFile("ocelot.json");
-
+            builder.Services.AddOcelot(builder.Configuration);
+            
             var app = builder.Build();
 
-            // Enable Authentication and Authorization Middleware
+            // Add Ocelot to application
+            app.UseOcelot().Wait();
+
             app.UseAuthentication();
+
             app.UseAuthorization();
-
-            // Use Ocelot middleware for routing
-            await app.UseOcelot();
-
-            // This secured endpoint is for component tests.
-            app.MapGet("/secured-endpoint", [Authorize] () => Results.Ok("You are authorized!"));
-
-            app.MapGet("/generate-token", (IConfiguration config) => {
-                var token = GenerateJwtToken(0, "admin", config);
-                return Results.Ok(new { Token = token });
-            });
-
+          
             app.Run();
-        }
-
-        // Method to generate JWT token
-        public static string GenerateJwtToken(int userid, string username, IConfiguration configuration)
-        {
-            // Retrieve JWT settings from configuration
-            var secretKey = configuration["JwtSettings:SecretKey"];
-
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-            var claims = new[]
-            {
-                new Claim(JwtRegisteredClaimNames.Sub, userid.ToString()),
-                new Claim(ClaimTypes.Name, username),
-                new Claim(JwtRegisteredClaimNames.Iat, ((int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds).ToString()) // Current time as Unix timestamp
-            };
-
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.UtcNow.AddMinutes(525600),  // Token is valid for 1 year
-                signingCredentials: credentials);
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
---
+++ AuthenticationService/Program.cs
@@ -0,0 +1,67 @@
+namespace AuthenticationService
+{
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using System.Text;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.IdentityModel.Tokens;
+    using Microsoft.AspNetCore.Builder;
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthentication();
+            builder.Services.AddAuthorization();
+
+            var app = builder.Build();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapPost("/login", () => {
+                var token = CreateToken();
+                return Results.Ok(token);
+            });
+
+            app.Run();
+        }
+
+        //Generate the token the user will use to sign in
+        public static AuthenticationToken CreateToken()
+        {
+            //The secret key that is used to generate tokens
+            const string SecurityKey = "MyClientSecretThatIsDefinitelyNotTooShort";
+            
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecurityKey));
+            var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            //Claims describe what a user is allowed and not allowed to do
+            var claims = new List<Claim>
+            {
+                new Claim("scope", "/api/login.write"),
+                new Claim("scope", "/api/users.write"),
+                new Claim("scope", "/api/users.write"),
+                new Claim("scope", "/api/users/{id}.read"),
+                new Claim("scope", "/api/users/{id}/follow/{followedId}.write"),
+                new Claim("scope", "/api/tweets.write"),
+                new Claim("scope", "/api/tweets/{id}/like.write"),
+                new Claim("scope", "/api/timeline/{userId}.read")
+            };
+
+            var tokenOptions = new JwtSecurityToken(
+                signingCredentials: signingCredentials, claims: claims
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions); 
+            var authToken = new AuthenticationToken
+            {
+                Value = tokenString
+            };
+            
+            return authToken;
+        }   
+    }
+}
